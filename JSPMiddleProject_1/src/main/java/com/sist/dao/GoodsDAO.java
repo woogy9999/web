ce4a6a3bc0651e7ab787603fbcd849e5fdc50316
microsoft <microsoft@sist>
2025-02-17 16:00:03 +0900

    1111

--- JSPMiddleProject_1/src/main/java/com/sist/dao/GoodsDAO.java
+++ JSPMiddleProject_1/src/main/java/com/sist/dao/GoodsDAO.java
@@ -1,5 +1,4 @@
 package com.sist.dao;
-
 import java.util.*;
 
 import org.apache.ibatis.io.Resources;
@@ -9,39 +8,31 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import com.sist.vo.GoodsVO;
 
 import java.io.*;
-
 // 2.XX => iBatis 3.XX => MyBatis 
 // open source      google에서 인수 
 public class GoodsDAO {
-	private static SqlSessionFactory ssf;
-	// 관리자 => 연결 => SQL문장 처리 => XML파일을 파싱 요청
-	static {
-		try {
-			Reader reader = Resources.getResourceAsReader("Config.xml");
-			
-			try {
-			    ssf = new SqlSessionFactoryBuilder().build(reader);
-			    if (ssf == null) {
-			        System.out.println("SqlSessionFactory is null");
-			    } else {
-			        System.out.println("SqlSessionFactory is created successfully");
-			    }
-			} catch (Exception e) {
-			    e.printStackTrace();  // 예외 발생 시 처리
-			    System.out.println("Failed to create SqlSessionFactory");
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	// 기능 처리
-	public static List<GoodsVO> goodsListData(Map map) {
-		return ssf.openSession().selectList("goodsListData", map);
-	}
-
-	public static int goodsTotalPage() {
-		return ssf.openSession().selectOne("goodsTotalPage");
-	}
-
+   private static SqlSessionFactory ssf;
+   // 관리자 => 연결 => SQL문장 처리 => XML파일을 파싱 요청 
+   static 
+   {
+	   try
+	   {
+		   Reader reader=Resources.getResourceAsReader("Config.xml");
+		   ssf=new SqlSessionFactoryBuilder().build(reader);
+	   }catch(Exception ex) 
+	   {
+		   ex.printStackTrace();
+	   }
+   }
+   
+   // 기능 처리 
+   public static List<GoodsVO> goodsListData(Map map)
+   {
+	   return ssf.openSession().selectList("goodsListData",map);
+   }
+   public static int goodsTotalPage()
+   {
+	   return ssf.openSession().selectOne("goodsTotalPage");
+   }
+   
 }
--- JSPMiddleProject_2/src/main/java/com/sist/dao/FoodDAO.java
+++ JSPMiddleProject_2/src/main/java/com/sist/dao/FoodDAO.java
@@ -1,5 +1,67 @@
 package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.sist.vo.FoodVO;
+
+import java.io.*;
 
 public class FoodDAO {
-
+	   private static SqlSessionFactory ssf;
+	// XML 파싱
+	// 1. dom => xml용 트리형태로 저장후 처리
+	// 2. sax= 
+	
+	static
+	{
+		try {
+			// XML 파일 읽기 => Config.xml => mapper
+			Reader reder=Resources.getResourceAsReader("Config.xml");
+			ssf=new SqlSessionFactoryBuilder().build(reder);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	/*
+	 * 
+	 * 
+	 * <select id="foodListData" resultSets="FoodVO">
+ 		SELECT fno,name,poster,num
+ 		FROM (SELECT fno,name,poster,rownum as num
+ 		FROM (SELECT fno,name,poster
+ 		FROM food_menupan ORDER BY fno ASC))
+ 		WHERE num BETWEEN #{start} AND #{end}
+ 	</select>
+ 	<!--  
+ 	
+ 	private int fno,hit;
+	private double score;
+	private String name,type,phone,address,theme,poster,image,time,parking,content,price;
+	 -->
+ 	<select id="foodTotalPage" resultType="int">
+ 		SELECT CEIL(COUNT(*)/12.0) FROM food_menupan
+ 	</select>
+ 	
+ 	<update id="hitIncrement" parameterType="int">
+		UPDATE food_menupan SET 
+		hit=hit+1 
+		<include refid="where-fno"/>
+	
+	</update>
+ 	<select id="foodDetailData" resultType="FoodVO" parameterType="int">
+ 	 SELECT name,type,phone,address,theme,poster,image,time,parking,content,price,score,hit 
+	 FROM food_menupan 
+	<include refid="where-fno"/>
+ 	
+ 	</select>
+ 	
+	 */
+	public static FoodVO foodDetailData(int fno) {
+		ssf.openSession().update("hitIncrement",fno);
+		return ssf.openSession().selectOne("foodDetailData",fno);
+	}
 }
